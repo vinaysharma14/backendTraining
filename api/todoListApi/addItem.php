3ad99702ea211c3f1eb26fbdcddec0e00753b30e
vinaysharma14 <vinay@geekyants.com>
2019-03-12 11:13:51 +0530

    Items Added Successfully

---
+++ api/todoListApi/addItem.php
@@ -0,0 +1,37 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+require_once("connectDatabase.php");
+$connection=db();
+
+
+$jsonContent=json_decode(file_get_contents('php://input'),true);
+$value=$jsonContent["name"];
+
+$query="INSERT into taskTable (task,isCompleted) values ('$value',0)";
+if ($connection->query($query) === TRUE) 
+    $id = $connection->insert_id;
+else 
+    echo "Error";
+$connection->close();
+
+class todoItem
+{
+    public $id;
+    public $task;
+    public $isCompleted;
+    public function setValues($x,$y,$z)
+    {
+        $this->id=$x;
+        $this->task=$y;
+        $this->isCompleted=$z;
+    }
+}
+
+$todoObj=new todoItem();
+$todoObj->setValues($id,$value,0);
+
+$todoJSON=json_encode($todoObj);
+echo $todoJSON;
+?>
